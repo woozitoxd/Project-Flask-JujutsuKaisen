63990355c92af88127f3d3ab81bfa685b45cfc65
Elias Contrera <wooziecentury@gmail.com>
2025-09-17 16:21:54 -0300

    Agregado de OS
    Útil para subir el proyecto a RENDER

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, os
 
 app = Flask(__name__)
 
@@ -176,4 +176,4 @@ def home():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
